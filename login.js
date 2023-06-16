b4c497ddc1af973e17c8fd04ac6ca02a0ba3ecda
masif2002 <masif2002@outlook.com>
2023-06-16 18:48:16 +0530

    Feat: Submit on enter key

--- login.js
+++ login.js
@@ -1,9 +1,17 @@
 import { server } from "./url.js";
 
 const nextPage = () => {
+    // Removes focus from input field
+    document.getElementById('username').blur()
+
     addMoveOutTransitionEffect();
 
     addPasswordField();
+    // Shift focus to password field
+    document.getElementById('password').focus({
+        preventScroll: true
+    })
+
     addMoveInTransitionEffect();
 
     addButton('Back', 'M10 12.796V3.204L4.519 8 10 12.796zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z');
@@ -61,9 +69,16 @@ const addPasswordField = () => {
     password.name = "password";
     password.id = "password";
     password.placeholder = "Enter password";
+
+    password.onkeydown = async (event) => {
+        if (event.key == 'Enter') {
+            event.preventDefault()
+            await login()
+
+        }
+    }
     
-    document.getElementById('credentials-form').appendChild(password)
-    
+    document.getElementById('credentials-form').appendChild(password)    
 }
 
 const addButton = (buttonType, pathForSvg) => {
@@ -161,17 +176,25 @@ document.addEventListener("click", submitBtnListener)
 async function submitBtnListener(event){
     var element = event.target;
     if(element.id == 'submit' && element.type == "submit"){
+        await login()
+    }
+}
 
-        try {
-            var status = await sendFormData()
-        } catch (error) {
-            alert(error)
-        } finally {
-            if (status == 200) {
-                window.location.href = './index.html'
-            }
+const login = async () => {
+    try {
+        var status = await sendFormData()
+    } catch (error) {
+        alert(error)
+    } finally {
+        if (status == 200) {
+            window.location.href = './index.html'
         }
+    }
+}
 
-
+document.getElementById('username').onkeydown = (event) => {
+    if (event.key == 'Enter') {
+        event.preventDefault()
+        nextPage()
     }
 }
